577ac75424087f75a27370ac0658cd19df09dc6f
makerchoice <1774151675@qq.com>
2020-03-04 23:50:55 +0800

    update

--- a.c
+++ a.c
@@ -1,20 +1,24 @@
-#include<stdio.h>
+#include<stdio.h>//递归冒泡排序(从小到大） 
 #define N 1000
 #include <stdlib.h>
 #include <time.h>
-int fun(int a[],int i,int m)
+int count=0;//比较次数 
+int fun(int a[],int i,int m)//递归函数 
 {
-	int t;
-	if(a[i]<a[m])
-		{
-			t=a[i];
-			a[i]=a[m];
-			a[m]=t;
-		}
+	int t;	
 	if(m==i)
 		return a[i];
 	else
-		return fun(a,i,m-1);
+	{ 	
+		if(a[i]>a[m]) 
+		{
+				t=a[i];
+				a[i]=a[m];
+				a[m]=t;
+		}
+		count++;
+		return fun(a,i,m-1);//i不变，m--；实现 m-i-1次a[i]和a[m]比较 
+	}	
 	
 }
 void main()
@@ -22,19 +26,19 @@ void main()
 	int a[N],m,i,j,t;
 	printf("请输入要排序的数目(m<1000):");
 	scanf("%d",&m);
-	printf("请输入要排序的数:");
-	srand(time(NULL));
+	srand(time(NULL));// 设置一个随机种子
 	for(i=0;i<m;i++)
 	{
-		a[i]=rand()%1000+1; 
+		a[i]=rand()%1000+1; //随机产生 1~1000的数 赋予a[i] 
 	}
-	for(i=1;i<m;i++)
-		printf("%d ",a[i]);
+	for(i=0;i<m;i++)
+		printf("%d ",a[i]);//排序前 
 	printf("\n");
-	printf("从大到小排序:"); 
+	printf("从小到大排序:"); 
 	for(i=0;i<m;i++)
 	{
-		printf("%d ",fun(a,i,m-1));
+		printf("%d ",fun(a,i,m-1));//排序后 
 	}
+	printf("一共比较了：%d次",count);
 	
 }
